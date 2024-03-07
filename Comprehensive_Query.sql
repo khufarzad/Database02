f873478e43da318b87d201e959d9c1f80464729f
khufarzad <160780389+khufarzad@users.noreply.github.com>
2024-03-07 16:38:56 +0330

    Insert Data Into Table Query

--- Comprehensive_Query.sql
+++ Comprehensive_Query.sql
@@ -14,7 +14,6 @@ USE database_name;
 
 
 -- Create Table in Used Database
-
 -- CREATE TABLE table_name(
 -- 	coll_1 data_type,
 --     coll_2 data_type,
@@ -23,7 +22,7 @@ USE database_name;
 
 CREATE TABLE Students(
 	Student_ID INT NOT NULL,
-    First_Name VARCHAR(20),
+	First_Name VARCHAR(20),
     Last_Name VARCHAR(20),
     Score INT,
     UNIQUE(Student_ID)
@@ -31,6 +30,18 @@ CREATE TABLE Students(
 
 
 -- Insert Data In Table
+INSERT INTO Students VALUES
+	(1, 'Zahra', 'Rezaei', 28),
+    (2, 'Marziyeh', 'Rezaei', 58),
+    (3, 'Amin', 'Modanloo', 46),
+    (4, 'Hossein', 'Abazari', 55),
+    (5, 'Mahdi', 'Zolfaghari', 50),
+    (6, 'Fatemeh', 'Safadoust', 49),
+    (7, 'Elmira', 'Hajji', 51),
+    (8, 'Hiva', 'Yarandi', 80),
+    (9, 'Amir', 'Jamshid', 32),
+    (10, 'Mojtaba', 'Karimi', 56);
 
+INSERT INTO Students VALUES (11, 'Zahra', 'Asadollahi', 96);    
 
 
